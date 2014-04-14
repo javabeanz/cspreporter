c962cb006947ac2e9e5c82683a762f5c1b645098
javabean <java.development@gmail.com>
2014-04-14 21:08:56 +0200

    added report service

--- src/java/main/org/owasp/csp/domain/CSPReport.java
+++ src/java/main/org/owasp/csp/domain/CSPReport.java
@@ -3,8 +3,12 @@ package org.owasp.csp.domain;
 import java.io.Serializable;
 
 import org.codehaus.jackson.annotate.JsonProperty;
+import org.mongojack.ObjectId;
 
 public class CSPReport implements Serializable {
+
+	@ObjectId
+	public String _id;
 
 	@JsonProperty("csp-report")
 	public Report report = new Report();
--- src/java/main/org/owasp/csp/rest/CSPRestReporter.java
+++ src/java/main/org/owasp/csp/rest/CSPRestReporter.java
@@ -1,5 +1,8 @@
 package org.owasp.csp.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -7,6 +10,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.mongojack.DBCursor;
 import org.mongojack.JacksonDBCollection;
 import org.owasp.csp.domain.CSPReport;
 
@@ -24,7 +28,8 @@ public class CSPRestReporter {
 		JacksonDBCollection<CSPReport, String> jsoncoll = JacksonDBCollection
 				.wrap(coll, CSPReport.class, String.class);
 		jsoncoll.insert(report);
-		System.out.println("inserted " + jsoncoll);
+		System.out.println("inserted report that violates : "
+				+ report.report.violatedDirective);
 		return report;
 	}
 
@@ -37,9 +42,17 @@ public class CSPRestReporter {
 	@GET
 	@Path("/report")
 	@Produces(MediaType.APPLICATION_JSON)
-	public long report() {
+	public List<CSPReport> report() {
 		DBCollection coll = CSPApplication.db.getCollection("testCollection");
-		return coll.getCount();
+		JacksonDBCollection<CSPReport, String> jsoncoll = JacksonDBCollection
+				.wrap(coll, CSPReport.class, String.class);
+		DBCursor<CSPReport> cursor = jsoncoll.find();
+		List<CSPReport> reports = new ArrayList<>();
+		while (cursor.hasNext()) {
+			CSPReport o = cursor.next();
+			reports.add(o);
+			// System.out.println(o);
+		}
+		return reports;
 	}
-
 }
